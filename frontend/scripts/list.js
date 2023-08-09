a968b2be952b6fbbe6e458d3c60231617cd26602
Chandan2812 <chandanpms@gmail.com>
2023-08-09 22:45:55 +0530

    done

--- frontend/scripts/list.js
+++ frontend/scripts/list.js
@@ -39,11 +39,59 @@ function displayPlaylist(playlists) {
             <img src="${list.Poster}" alt="${list.Title}" id="image">
             <h2 id="title">${list.Title}</h2>
             <p id="year">${list.Year}</p>
-            
+            <button id="add" onClick="add()">Add to Playlist</button>
           `;
           
           listContainer.appendChild(movieElement);
         }
     }
    
+  }
+
+
+  function add(){
+    const logedInUserID = localStorage.getItem('logedInUserID') || null
+    if (!logedInUserID) {
+      alert(' Kindly Login First')
+      return
+    }
+    const Poster=document.getElementById("image").src
+    const Title=document.getElementById("title").innerText
+    const Year=document.getElementById("year").innerText
+    
+  
+    const payload={
+      userId:logedInUserID,
+      Poster,
+      Title,
+      Year
+    }
+  
+    fetch(`${url}/playlist/add`,{
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(payload)
+      })
+      .then(res =>res.json())
+      .then(data=>{
+        console.log(data)
+        if(data.message!='Already present in the playlist'){
+        Swal.fire(
+          'Added to your playlist',
+          'See your Playlist',
+          'success'
+        )
+        }
+        else{
+          Swal.fire(
+            'Already available in the playlist',
+            '',
+            'error',
+          )
+        }
+      })
+    
+  
   }
